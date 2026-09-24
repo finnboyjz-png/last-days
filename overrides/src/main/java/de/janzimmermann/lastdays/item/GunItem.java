626f20db3541f94329a149edd0c6f4ff38ec52e5
finnboyjz-png <finnboy.jz@googlemail.com>
2026-09-24 12:37:11 +0200

    Fix mixed SoundEvent holder types in LAST DAYS 1.2 gun engine

--- overrides/src/main/java/de/janzimmermann/lastdays/item/GunItem.java
+++ overrides/src/main/java/de/janzimmermann/lastdays/item/GunItem.java
@@ -58,9 +58,13 @@ public final class GunItem extends Item {
         player.getCooldowns().addCooldown(gun, cooldownTicks);
         NoiseSystem.addNoise(sp, shotgun ? 11 : sniper ? 9 : smg ? 7 : 6);
 
-        var shotSound = shotgun ? SoundEvents.GENERIC_EXPLODE.value() : SoundEvents.CROSSBOW_SHOOT.value();
-        server.playSound(null, player.blockPosition(), shotSound,
-                SoundSource.PLAYERS, shotgun ? 0.55F : 0.85F, shotgun ? 1.35F : 0.78F);
+        if (shotgun) {
+            server.playSound(null, player.blockPosition(), SoundEvents.GENERIC_EXPLODE.value(),
+                    SoundSource.PLAYERS, 0.55F, 1.35F);
+        } else {
+            server.playSound(null, player.blockPosition(), SoundEvents.CROSSBOW_SHOOT,
+                    SoundSource.PLAYERS, 0.85F, 0.78F);
+        }
 
         Vec3 look = player.getLookAngle();
         if (shotgun) {
